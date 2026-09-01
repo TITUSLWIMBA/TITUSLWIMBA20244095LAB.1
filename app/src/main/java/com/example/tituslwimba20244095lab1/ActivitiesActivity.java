95833b1ca568b299fcc491db6683b283e3ecf91b
MR KAY <tituskay95@gmail.com>
2026-09-01 08:19:58 -0700

    ADD FIRST PROJECT

---
+++ app/src/main/java/com/example/tituslwimba20244095lab1/ActivitiesActivity.java
@@ -0,0 +1,12 @@
+package com.example.tituslwimba20244095lab1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivitiesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities);
+    }
+}
---
+++ app/src/main/java/com/example/tituslwimba20244095lab1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tituslwimba20244095lab1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.tituslwimba20244095lab1.R;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnTimetable = findViewById(R.id.btnTimetable);
+        Button btnNotices = findViewById(R.id.btnNotices);
+        Button btnAnnouncements = findViewById(R.id.btnAnnouncements);
+
+        if (btnTimetable != null) {
+            btnTimetable.setOnClickListener(v -> startActivity(new Intent(this, TimetableActivity.class)));
+        }
+        if (btnNotices != null) {
+            btnNotices.setOnClickListener(v -> startActivity(new Intent(this, NoticesActivity.class)));
+        }
+        if (btnAnnouncements != null) {
+            btnAnnouncements.setOnClickListener(v -> startActivity(new Intent(this, AnnouncementActivity.class)));
+        }
+    }
+}
